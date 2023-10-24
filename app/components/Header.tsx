af9bdf6f2c981f0dd3ff80392d6eb5fa538b3210
ZNP8b <znp8b00ff@gmail.com>
2023-10-25 01:05:20 +0300

    Ститлизовать первую кнопку у Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image"
+import Link from "next/link"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="w-fit flex gap-1">
+      <Link
+        className="flex bg-[#343A40] text-white justify-center items-center font-bold text-2xl uppercase gap-4 py-5 px-12"
+        href='/'>
+        <div>
+          <Image src="delivery_icon.svg" alt="delivery_icon" width={40} height={40} />
+        </div>
+        <span>Доставка</span>
+      </Link>
+    </div>
   )
 }
 
