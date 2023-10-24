8f130bea121519f3e8a70a13db7af494559841d3
ZNP8b <znp8b00ff@gmail.com>
2023-10-25 02:34:49 +0300

    Жоская верстка навбара жесть

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,17 +1,54 @@
 import Image from "next/image"
 import Link from "next/link"
 
+const links = [
+  {
+    name: 'Доставка',
+    url: 'delivery_icon.svg'
+  },
+  {
+    name: 'Самовывоз',
+    url: 'pedestrian-man.svg'
+  }
+]
+
+const sortLinks = ['Время доставки', 'Готовые блюда', 'Упакованные блюда']
+
 const Header = () => {
   return (
-    <div className="w-fit flex gap-1">
-      <Link
-        className="flex bg-[#343A40] text-white justify-center items-center font-bold text-2xl uppercase gap-4 py-5 px-12"
-        href='/'>
-        <div>
-          <Image src="delivery_icon.svg" alt="delivery_icon" width={40} height={40} />
-        </div>
-        <span>Доставка</span>
-      </Link>
+    <div className="flex justify-around gap-[2px] flex-col text-xl md:flex-row md:text-base lg:text-xl 2xl:text-2xl">
+      {
+        links.map((item) => (
+          <Link
+            className="flex bg-[#343A40] text-white justify-center items-center 
+          font-bold uppercase gap-3 flex-1 py-4"
+            href='/'>
+            <div className="md:hidden xl:flex">
+              <Image src={item.url} alt="icon" width={40} height={40} />
+            </div>
+            <span>{item.name}</span>
+          </Link>
+        ))
+      }
+
+      <span
+        className="flex flex-1 justify-center items-center md:text-sm lg:text-xl 2xl:text-2xl
+         text-white text-center bg-[#343A40] text-[#F9F9F9]/80 py-4">
+        Сортировать по
+      </span>
+
+      {
+        sortLinks.map((item) => (
+          <Link
+            className="flex flex-1 bg-[#343A40] text-white justify-center items-center 
+            text-center py-4"
+            href='/'>
+
+            <span>{item}</span>
+          </Link>
+        ))
+      }
+
     </div>
   )
 }
