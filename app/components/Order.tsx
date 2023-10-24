b4ad00334b05389ed191aad23aeb9fb22166df9a
ZNP8b <znp8b00ff@gmail.com>
2023-10-25 02:34:59 +0300

    Заглушка элемента заказа

---
+++ app/components/Order.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Order = () => {
+  return (
+    <div className='bg-red-100 w-[595px] h-[748px] flex items-center justify-center text-5xl text-center m-5'>
+      <span>Заказ</span>
+    </div>
+  )
+}
+
+export default Order
